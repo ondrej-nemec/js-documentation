e4f84ce236dc8e3289a04b29786d9805f1af2f2d
Ondřej Němec <nikdynikde@gmail.com>
2022-07-01 22:45:05 +0200

    ament

---
+++ v5/examples/example-resources/demos.js
@@ -0,0 +1,9 @@
+document.querySelector("#js-loaded").remove();
+	/*console.log("JS inline script executed");
+	try {
+		document.addEventListener('themeContentAdded', function() {
+			document.getElementById("head-script").style['background-color'] = "green";
+		});
+	} catch (e) {
+		console.error("head", e);
+	}*/
